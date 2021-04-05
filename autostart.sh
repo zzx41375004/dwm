1d10f49b68ce82868a5e72e1b0f42531052b3cec
朱志先 <8757021+zzx2070276239@user.noreply.gitee.com>
2021-04-05 23:16:31 +0800

    font

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+~/dwm/scripts/dwm-status.sh
 ~/dwm/scripts/exchangeCaps.sh
 ~/dwm/scripts/inverse-scroll.sh
--- config.h
+++ config.h
@@ -10,8 +10,10 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "monospace:size=16";
+/* static const char *fonts[]          = { "monospace:size=16" }; */
+/* static const char dmenufont[]       = "monospace:size=16"; */
+static const char *fonts[]          = { "Fira Code Nerd Font Mono:size=16" };
+static const char dmenufont[]       = "Fira Code Nerd Font Mono:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -39,15 +41,15 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "Tile",      tile },    /* first entry is default */
+	{ "平铺",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "Mono",      monocle },
+	{ "全屏",      monocle },
 };
 
 /* key definitions */
