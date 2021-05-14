d68bd37a03887895b5480220acd34afbc95b27b2
朱志先 <8757021+zzx2070276239@user.noreply.gitee.com>
2021-05-14 20:09:29 +0800

    514

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-xrandr --auto --output HDMI-A-0 --same-as eDP --size 1920*1080
-xrandr --auto --output DisplayPort-0 --same-as eDP --size 1920*1080
+xrandr --output HDMI-A-0 --same-as eDP --mode 1920x1080
+xrandr --output DisplayPort-0 --same-as eDP --mode 1920x1080
 # xrandr --auto --output DisplayPort-0 --same-as eDP --size 1920*1080
 fcitx5&
 ~/dwm/scripts/dwm-status.sh &
